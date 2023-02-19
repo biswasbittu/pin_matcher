e9e93549ac0a881a41d9b4200f4c086303d2a042
biswasbittu <biswasbittu7415@yahoo.com>
2023-02-19 14:45:26 +0530

    pin matcher function done

---
+++ javaScript/app.js
@@ -0,0 +1,53 @@
+// function generatePin(){
+//    const random= Math.round(Math.random()*10000)
+//    return random;
+// }
+
+document.getElementById('generate-pin').addEventListener('click',function(){
+    const pin=getPin();
+
+    const displayShow=document.getElementById('display-pin');
+
+      displayShow.value=pin;
+
+})
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+function getPin(){
+    const pin =generatePin();
+    const pinString =pin + '';
+    if(pinString.length === 4){
+        return pin;
+    }
+    else{
+    
+        return getPin();
+    }
+}
+
+
+
+
+function generatePin(){
+    const random=Math.round(Math.random()*10000)
+    return random;
+}
